b03641ed92bb8e323c51fb03a5e11c5e0cf51f7b
Mo Amer <moamrr@gmail.com>
2025-10-17 19:58:32 +0100

    Remove all placeholder artworks and references

--- src/sketches/index.js
+++ src/sketches/index.js
@@ -3,27 +3,6 @@ import sketch_002_morse from "./002_morse";
 import sketch_003_morse_enhanced from "./003_morse_enhanced";
 import sketch_004_morse_vera from "./004_morse_vera";
 import sketch_005_vera_lines from "./005_vera_lines";
-import sketch_006_placeholder_signal from "./006_placeholder_signal";
-import sketch_007_placeholder_signal from "./007_placeholder_signal";
-import sketch_008_placeholder_signal from "./008_placeholder_signal";
-import sketch_009_placeholder_signal from "./009_placeholder_signal";
-import sketch_010_placeholder_signal from "./010_placeholder_signal";
-import sketch_011_placeholder_signal from "./011_placeholder_signal";
-import sketch_012_placeholder_signal from "./012_placeholder_signal";
-import sketch_013_placeholder_signal from "./013_placeholder_signal";
-import sketch_014_placeholder_signal from "./014_placeholder_signal";
-import sketch_015_placeholder_signal from "./015_placeholder_signal";
-import sketch_016_placeholder_signal from "./016_placeholder_signal";
-import sketch_017_placeholder_signal from "./017_placeholder_signal";
-import sketch_018_placeholder_signal from "./018_placeholder_signal";
-import sketch_019_placeholder_signal from "./019_placeholder_signal";
-import sketch_020_placeholder_signal from "./020_placeholder_signal";
-import sketch_021_placeholder_signal from "./021_placeholder_signal";
-import sketch_022_placeholder_signal from "./022_placeholder_signal";
-import sketch_023_placeholder_signal from "./023_placeholder_signal";
-import sketch_024_placeholder_signal from "./024_placeholder_signal";
-import sketch_025_placeholder_signal from "./025_placeholder_signal";
-import sketch_026_placeholder_signal from "./026_placeholder_signal";
 
 const sketches = {
   "001_example": sketch_001_example,
@@ -31,27 +10,6 @@ const sketches = {
   "003_morse_enhanced": sketch_003_morse_enhanced,
   "004_morse_vera": sketch_004_morse_vera,
   "005_vera_lines": sketch_005_vera_lines,
-  "006_placeholder_signal": sketch_006_placeholder_signal,
-  "007_placeholder_signal": sketch_007_placeholder_signal,
-  "008_placeholder_signal": sketch_008_placeholder_signal,
-  "009_placeholder_signal": sketch_009_placeholder_signal,
-  "010_placeholder_signal": sketch_010_placeholder_signal,
-  "011_placeholder_signal": sketch_011_placeholder_signal,
-  "012_placeholder_signal": sketch_012_placeholder_signal,
-  "013_placeholder_signal": sketch_013_placeholder_signal,
-  "014_placeholder_signal": sketch_014_placeholder_signal,
-  "015_placeholder_signal": sketch_015_placeholder_signal,
-  "016_placeholder_signal": sketch_016_placeholder_signal,
-  "017_placeholder_signal": sketch_017_placeholder_signal,
-  "018_placeholder_signal": sketch_018_placeholder_signal,
-  "019_placeholder_signal": sketch_019_placeholder_signal,
-  "020_placeholder_signal": sketch_020_placeholder_signal,
-  "021_placeholder_signal": sketch_021_placeholder_signal,
-  "022_placeholder_signal": sketch_022_placeholder_signal,
-  "023_placeholder_signal": sketch_023_placeholder_signal,
-  "024_placeholder_signal": sketch_024_placeholder_signal,
-  "025_placeholder_signal": sketch_025_placeholder_signal,
-  "026_placeholder_signal": sketch_026_placeholder_signal,
 };
 
 export default sketches;
